3f4e0ac865bac55d78ac8b86e0453aa561dcacaa
ihabah1 <ihab.ah1@gmail.com>
2020-09-27 03:06:06 +0300

    Updates

---
+++ precache-manifest.469fb9e5f93451bd6e433b3cb4e512f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "828506f13c9ff2d05743",
+    "url": "/static/js/main.828506f1.chunk.js"
+  },
+  {
+    "revision": "111e4f6d0b61044c6890",
+    "url": "/static/js/2.111e4f6d.chunk.js"
+  },
+  {
+    "revision": "828506f13c9ff2d05743",
+    "url": "/static/css/main.98560cf0.chunk.css"
+  },
+  {
+    "revision": "2a2832c6078231b34e339750a2b09d0a",
+    "url": "/index.html"
+  }
+];
